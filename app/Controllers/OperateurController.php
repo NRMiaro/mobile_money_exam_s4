f249a919d3d9a7b01f7228a0e0028be574a8697f
NRMiaro <miarotools@gmail.com>
2026-07-20 13:42:34 +0300

    ok

--- app/Controllers/OperateurController.php
+++ app/Controllers/OperateurController.php
@@ -1,24 +1,96 @@
-<?php 
+<?php
+
 namespace App\Controllers;
 
 use App\Models\UtilisateurModel;
+use App\Services\BaremeService;
 use App\Services\PrefixeService;
 use App\Services\TransactionService;
 use App\Services\UtilisateurService;
 
-class OperateurController extends BaseController{
+class OperateurController extends BaseController
+{
 
-    public function dashboard(){
+    private BaremeService $baremeService;
+
+    public function __construct()
+    {
+        $this->baremeService = new BaremeService();
+    }
+
+    public function dashboard()
+    {
         $transactionService = new TransactionService();
         $prefixeService = new PrefixeService();
         $utilisateurService = new UtilisateurService();
-        $data =[
+        $data = [
             'totalGains' => $transactionService->getTotalGains(),
             'totalPrefixes' => $prefixeService->getTotalPrefixes(),
             'nombreClientsActifs' => $utilisateurService->countClientsActifs()
         ];
 
-        return view("operateur/dashboard",$data);
+        return view("operateur/dashboard", $data);
     }
 
+
+
+    // ---------- OPERATEUR ----------
+    public function operateurDashboard(): string
+    {
+        return view('operateur/dashboard');
+    }
+
+    public function operateurPrefixesIndex(): string
+    {
+        return view('operateur/prefixe/index');
+    }
+
+    public function operateurPrefixesCreate(): string
+    {
+        return view('operateur/prefixe/create');
+    }
+
+    public function operateurBaremesIndex(): string
+    {
+        return view('operateur/bareme/index', [
+            'baremesDepot' => $this->baremeService->getBaremesDepot(),
+            'baremesRetrait' => $this->baremeService->getBaremesRetrait(),
+            'baremesTransfert' => $this->baremeService->getBaremesTransfert(),
+        ]);
+    }
+
+    public function operateurBaremesCreate(): string
+    {
+        return view('operateur/bareme/create');
+    }
+
+    public function operateurBaremesEdit($id)
+    {
+        return view('operateur/bareme/edit', [
+            'contexte' => $this->baremeService
+                ->getContexteModification($id)
+        ]);
+    }
+
+    public function operateurBaremesUpdate($id)
+    {
+        $result = $this->baremeService->updateBareme(
+            $id,
+            (int)$this->request->getPost('montant_min'),
+            (int)$this->request->getPost('montant_max'),
+            (float)$this->request->getPost('frais')
+        );
+
+
+        if ($result) {
+            return redirect()
+                ->to('/operateur/baremes')
+                ->with('success', 'Barème modifié avec succès');
+        }
+
+
+        return redirect()
+            ->back()
+            ->with('error', 'Erreur modification');
+    }
 }
